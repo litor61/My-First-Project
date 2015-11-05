912f0208a77c035bcf7b0d5195793479dea9e322
HackDeveloperUA <devios161@gmail.com>
2015-11-05 08:49:23 +0300

    Initial Commit

---
+++ testFirst/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testFirst
+//
+//  Created by MD on 05.11.15.
+//  Copyright (c) 2015 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testFirst/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testFirst
+//
+//  Created by MD on 05.11.15.
+//  Copyright (c) 2015 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
